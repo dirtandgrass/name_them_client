23caeedd3b5fa4649d31e88a7c868f2cf94c5b56
Lucas Stephenson <lstephenson@cegep-heritage.qc.ca>
2023-08-13 21:49:43 -0400

    results fixes

--- api/rating.ts
+++ api/rating.ts
@@ -5,37 +5,38 @@ import headerauth, { AuthUser } from '../backend/middleware/headerauth';
 import { Sex } from '../backend/model/name';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-
+  await headerauth(req, res);
+  // a user is required for rating functionality
+  if (!AuthUser || !AuthUser.user_id) { res.json({ message: "not logged in", success: false }); return; }
   const name_id = parseInt(req.query.name_id as string);
 
-  if (name_id) {
-    await headerauth(req, res);
-    // a user is required for rating functionality
-    if (!AuthUser || !AuthUser.user_id) { res.json({ message: "not logged in", success: false }); return; }
 
-    if (req.method === "POST") {
 
-      // rate name in a group
-      // body: name_id, rating, group_id
-      const result = await Rating.rateName(name_id, req.body.rating, req.body.group_id);
-      res.json(result);
-      return;
 
-    } else if (req.method === "GET") {
+  if (req.method === "POST") {
 
-      const { group_id, action = undefined, count = undefined, sex = Sex.all } = req.query;
-      // get top rated names
-      if (action === "top") {
-        const result = await Rating.getTopRatedNames(parseInt(group_id as string), parseInt(count as string) ?? undefined, sex as Sex | undefined);
+    // rate name in a group
+    // body: name_id, rating, group_id
+    const result = await Rating.rateName(name_id, req.body.rating, req.body.group_id);
+    res.json(result);
+    return;
 
-        res.json(result);
-        return;
-      }
+  } else if (req.method === "GET") {
 
-      // get rating for name
-      const result = await Rating.getRating(name_id, req.body?.group_id);
+
+    const { group_id, action = undefined, count = undefined, sex = Sex.all } = req.query;
+    // get top rated names
+    if (action === "top") {
+      const result = await Rating.getTopRatedNames(parseInt(group_id as string), parseInt(count as string) ?? undefined, sex as Sex | undefined);
+
       res.json(result);
       return;
     }
+
+    // get rating for name
+    const result = await Rating.getRating(name_id, req.body?.group_id);
+    res.json(result);
+    return;
   }
+
 }
